c9af31cf352843257fe5dd03b5762d15374b6089
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-09-19 11:24:26 +0200

    made some comments
    comments to know what I wrote

--- index.php
+++ index.php
@@ -7,8 +7,11 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Opendag Software Developer</title>
 </head>
+<!-- The body is the whole page-->
 <body>
+    <!--This class is a layout that control how the page look like -->
     <div class ="layout">
+    <!--This is the header the top and small part of the website -->
     <header>
         <nav>
             <div class="Top">
@@ -17,10 +20,12 @@
             </div>
         </nav>
     </header>
+    <!--This is the main the mid and biggest part of the website -->
     <main>
         <h1>Welkom op de website van Curio Software Developer editie.</h1>
         <div class="img2"><img src="img/School.jpg" alt="school"></div>
     </main>
+    <!--This is the footer the bottom and a small part of the website -->
     <footer>
 
     </footer>
