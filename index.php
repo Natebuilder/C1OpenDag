dedf4d5cbb44f146e43d44ebdc1814ceed591938
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-09-22 16:07:02 +0200

    css updated

--- index.php
+++ index.php
@@ -6,65 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Opendag Software Developer</title>
-    <style>
-        .part1 { grid-area: header; }
-        .part2 { grid-area: menu; }
-        .part3 { grid-area: main; }
-        .part4 { grid-area: footer; }
-
-        .grid-container {
-        display: grid;
-        grid-template-areas:
-            'header header'
-            'menu menu'
-            'main main '
-            'footer footer';
-        gap: 10px;
-        background-color: #2196F3;
-        padding: 10px;
-        }
-
-        .grid-container > div {
-        background-color: rgba(255, 255, 255, 0.8);
-        text-align: center;
-        padding: 20px 0;
-        font-size: 30px;
-        }
-    </style>
 </head>
 <!-- The body is the whole page-->
 <body>
     <!--This class is a layout that control how the page look like -->
     <div class ="layout">
     <!--This is the header the top and small part of the website -->
-        <div class="part1">
-            <header>
-                <nav>
-                    <img src="img/Curio Logo.png" alt="Logo">
-                    <h1>Opendag Software Developer 350</h1>
-                </nav>
-            </header>
-        </div>
-        <!--This is the menu-->
-        <div class="part2">
-            <menu>
-                <h1>menu</h1>
-                <a href="#blok">Blok A en Blok B</a>
-            </menu>
-        </div>
-            <!--This is the main the mid and biggest part of the website -->
-        <div class="part3">
-            <main>
-                <h1>Welkom op de website van Curio Software Developer editie.</h1>
-                <img src="img/School.jpg" alt="school">
-            </main>
-        </div>
-            <!--This is the footer the bottom and a small part of the website -->
-        <div class="part4">
-            <footer>
-                <p>Copyright | Student: Nathan van den Heuvel | Jaar van maak: 2022</p>
-            </footer>
-        </div>
+        <header>
+            <nav>
+                <img src="img/Curio Logo.png" alt="Logo">
+                <h1>Opendag Software Developer 350</h1>
+            </nav>
+        </header>
+    <!--This is the menu-->
+        <menu>
+            <h1>menu</h1>
+            <a href="#blok">Blok A en Blok B</a>
+        </menu>
+    
+        <!--This is the main the mid and biggest part of the website -->
+        <main>
+            <h1>Welkom op de website van Curio Software Developer editie.</h1>
+            <img src="img/School.jpg" alt="school">
+        </main>
+        <!--This is the footer the bottom and a small part of the website -->
+        <footer>
+            <p>Copyright | Student: Nathan van den Heuvel | Jaar van maak: 2022</p>
+        </footer>
     </div>
 </body>
 </html>
