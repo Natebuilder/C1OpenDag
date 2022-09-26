eea55c72ddd27e2ff97f2a1e42f24d21be524354
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-09-26 12:47:40 +0200

    head gescheiden van index

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Opendag Software Developer</title>
-</head>
+<?php require_once 'head.php'; ?>
 <!-- The body is the whole page-->
 <body>
     <!--This is the header the top and small part of the website -->
