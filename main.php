3171ab79bb2ada8ea3a075a5800b1e593c3da8de
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-09-26 22:18:18 +0200

    Vakken gemaakt

--- main.php
+++ main.php
@@ -24,8 +24,103 @@
                 maken, aanpassen of verwijderen. Je leert ook het bouwen van meer complexe applicaties
                 in C#. C# is uitdagend, maar het is te doen. Als je deze eind feedbackmomenten wilt gaan halen
                 zou je meer uit te kast moeten halen om de feedbackmomenten te halen.
-
             </h2>
         </div>
         <h1>Vakken van deze opleiding</h1>
+        <div class="main-layout">
+            <div class="bold">
+                <h2>Native</h2>
+            </div>
+            <div class="bold">
+                <h2>Web</h2>
+            </div>
+            <h2>
+            Win/native is de applicatie kant. Je leert 
+            applicaties bouwen in eerst python en in latere 
+            blokken C#. Hiermee kan je simpele spellen bouwen 
+            of formulieren zoals je voornaam en achternaam.
+            </h2>
+            <h2>Er zijn heel wat vakken die je 
+                krijgt die je uiteindelijk helpt 
+                om je opleiding te halen. Je hebt 
+                bijvoorbeeld Web, en web is de vak 
+                waar je leert websites te bouwen die 
+                later webapps zijn. Webapps leer je 
+                in blok b of hoger.
+            </h2>
+            <div class="img1">
+                <img src="img/python.png" alt="python">
+            </div>
+            <div class="img2">
+                <img src="img/php.png" alt="php">
+            </div>
+            <div class="bold">
+                <h2>Nederlands</h2>
+            </div>
+            <div class="bold">
+                <h2>Engels</h2>
+            </div>
+            <h2>Bij Nederlands leer je onder andere begrijpt lezen, woordenschat,
+                grammatica kort om leerstof die je al eerder heb gehad. Maar je leert 
+                ook hoe je zakelijke brieven schrijft en die vaardigheid heb je nodig
+                voor je stage.
+            </h2>
+            <h2>Engels is ook een vak die je eerder heb gehad. Engels op het Mbo is anders 
+                dan dat je ben gewend. Er worden wel toetsen gemaakt, maar die maak je wanneer
+                het jou uitkomt geen repetities of overhoringen. Daarnaast verdiep je nog meer 
+                in het vak om in Engels nog beter te worden.
+            </h2>
+            <div class="bold">
+                <h2>Rekenen</h2>
+            </div>
+            <div class="bold">
+                <h2>Praktijk</h2>
+            </div>
+            <h2>Rekenen is met cijfers, procenten of verhoudingen en statieken. Je heb deze vaardigheden
+                nodig als je wilt gaan programmeren. Soms moet je bepaalde dingen uitrekenen en daarom 
+                is het handig als je goed kan rekenen.
+            </h2>
+            <h2>Praktijk bouw je projecten. Je bouwt de projecten in of een groepje of alleen.
+                Je leert samenwerken met medestudenten om samen een project te maken. Het project
+                bestaat uit het stof dat je bij Native en Web leert. Soms is het een project met alleen Native,
+                of alleen Web of beide.
+            </h2>
+            <div class="bold">
+                <h2>Algo</h2>
+            </div>
+            <div class="bold">
+                <h2>Keuzedelen</h2>
+            </div>
+            <h2>Algo is een nieuw vak waar je de bouw met if statements en loops goed oefenen.
+                Dit heb je nodig in alle programmeertalen.
+            </h2>
+            <h2>Tja keuzedelen het eerste keuzedeel is digitale vaardigheden. Dit kies je niet zelf.
+                Je maakt hier documenten, powerpoints en meer waarbij je digitale vaardigheden uitoefen.
+                Waarom school dit doet is, omdat na verwachting nieuwe studenten niet genoeg computer kennis 
+                hebben.
+            </h2>
+            <div class="bold">
+                <h2>studieloopbaanbegeleiding</h2>
+            </div>
+            <div class="bold">
+                <h2>portfolio</h2>
+            </div>
+            <h2>studieloopbaanbegeleiding is een soort mentor uur. Je spreekt met je studieloopbaanbegeleider 
+                met je klas over hoe het jaar verloopt, wat er beter kan en wat goed gaat, en of er dingen misschien
+                kunnen veranderen.
+            </h2>
+            <h2>Portfolio hoef je niet letterlijk een heel portfolio bij te houden. Het is eigenlijk het zelfde als bij studieloopbaanbegeleiding,
+                maar dan indivueel alleen met je SLB(studieloopbaanbegeleider)dus.
+            </h2>
+            <div class="bold">
+                <h2>praktijkondersteuning</h2>
+            </div>
+            <h2></h2>
+            <h2>Praktijkondersteuning leer je vaardigheden die je kan helpen of je praktijk opdrachten beter te maken.
+                Je leert vaardigheden zoals, hoe je goed plan of hoe je goed communiceert met je team enz..
+            </h2>
+            <div class="img3">
+                <img src="img/scrum.png" alt="scrum">
+            </div>
+        </div>
     </main>
